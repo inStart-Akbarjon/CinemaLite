e37294513f086f5194315aae0004ed887c32aae3
Akbarjon <akbarjonfayzulloev585@mail.com>
2026-02-06 17:13:46 +0500

    added_MovieCacheService

--- src/CinemaLite.Application/Extensions/RedisCache/DistributedCacheExtension.cs
+++ src/CinemaLite.Application/Extensions/RedisCache/DistributedCacheExtension.cs
@@ -1,38 +1,9 @@
-﻿using System.Text;
-using CinemaLite.Application.Models.Cache;
-using Microsoft.Extensions.Caching.Distributed;
-using Newtonsoft.Json;
-using StackExchange.Redis;
+﻿using StackExchange.Redis;
 
 namespace CinemaLite.Application.Extensions.RedisCache;
 
 public static class DistributedCacheExtension
 {
-    public static async Task<T?> GetOrSetAsync<T>(
-        this IDistributedCache cache, 
-        string cacheKey, 
-        Func<Task<T>> task)
-    {
-        var options = new DistributedCacheEntryOptions()
-        {
-            SlidingExpiration = TimeSpan.FromMinutes(5),
-        };
-
-        var cached = await cache.GetStringAsync(cacheKey);
-
-        if (cached != null)
-        {
-            return JsonConvert.DeserializeObject<T>(cached);
-        }
-
-        var movies = await task();
-
-        var bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(movies));
-        await cache.SetAsync(cacheKey, bytes, options);
-
-        return movies;
-    }
-
     public static async Task InvalidateAsync(this IConnectionMultiplexer redis, string registry)
     {
         var db = redis.GetDatabase();
---
+++ src/CinemaLite.Application/Services/Interfaces/RedisDistributedCache/IMovieCacheService.cs
@@ -0,0 +1,10 @@
+﻿using CinemaLite.Application.DTOs.Movie.Response;
+using CinemaLite.Application.DTOs.Pagination;
+
+namespace CinemaLite.Application.Services.Interfaces.RedisDistributedCache;
+
+public interface IMovieCacheService
+{
+    Task<PaginatedMovieList<GetAllMoviesResponse>?> GetAllMoviesFromCacheAsync(string cacheKey);
+    Task AddMoviesToCacheAsync(string cacheKey, PaginatedMovieList<GetAllMoviesResponse> movies);
+}
