d93e331ed310d603a8b5a1811351740eb6735381
Akbarjon <akbarjonfayzulloev2007@gmail.com>
2026-05-11 13:06:33 +0500

    fix

---
+++ src/CinemaLite.Domain/Models/AuditLogDetails.cs
@@ -0,0 +1,8 @@
+﻿namespace CinemaLite.Domain.Models;
+
+public class AuditLogDetails
+{
+    public string PropertyName { get; set; }
+    public string OldValue { get; set; }
+    public string NewValue { get; set; }
+}
---
+++ src/CinemaLite.Infrastructure/Database/Migrations/20260506084856_AddedAuditLogEntity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using CinemaLite.Domain.Models;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CinemaLite.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedAuditLogEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AuditLog",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    TableName = table.Column<string>(type: "text", nullable: false),
+                    EntityId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Action = table.Column<int>(type: "integer", nullable: false),
+                    ChangedBy = table.Column<string>(type: "text", nullable: false),
+                    ChangedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    AuditLogDetails = table.Column<List<AuditLogDetails>>(type: "jsonb", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuditLog", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuditLog");
+        }
+    }
+}
